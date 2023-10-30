167cf34d457c11611b2bee6861302c11be4a2b92
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-10-30 19:34:27 +0200

    commit

--- src/main/java/com/service/maintenance/controller/UtilisateurController.java
+++ src/main/java/com/service/maintenance/controller/UtilisateurController.java
@@ -2,6 +2,7 @@ package com.service.maintenance.controller;
 
 
 import com.service.maintenance.model.Utilisateur;
+import com.service.maintenance.repository.UtilisateurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +15,19 @@ public class UtilisateurController {
     @Autowired
     UtilisateurRepository utilisateurRepository;
 
-//    @GetMapping("/getuser")
-//    public ResponseEntity<Utilisateur> getuser(Utilisateur user){
-//        try {
-//            Utilisateur utilisateur = utilisateurRepository.findByEmailAndMdp(user);
-//            if (utilisateur==null) {
-//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//            }
-//            return new ResponseEntity<>(utilisateur, HttpStatus.OK);
-//        } catch (Exception e) {
-//            System.out.println("Message:"+e.getMessage());
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
+    @GetMapping("/getuser")
+    public ResponseEntity<Utilisateur> getuser(Utilisateur user){
+        try {
+            Utilisateur utilisateur = utilisateurRepository.findByEmailAndMdp(user);
+            if (utilisateur==null) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(utilisateur, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println("Message:"+e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     //    Insertion nouvelle type entretien
     @PostMapping("/insertuser")
     public ResponseEntity<Utilisateur> insertutilisateur(@RequestBody Utilisateur v) {
--- src/main/java/com/service/maintenance/repository/UtilisateurRepository.java
+++ src/main/java/com/service/maintenance/repository/UtilisateurRepository.java
@@ -3,5 +3,8 @@ package com.service.maintenance.repository;
 import com.service.maintenance.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.rmi.CORBA.Util;
+
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
+    Utilisateur findByEmailAndMdp(Utilisateur user);
 }
